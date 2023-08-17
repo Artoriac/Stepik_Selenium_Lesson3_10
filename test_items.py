92011e7d54ee8527815de0d1251a92bfd65f42b3
Yaroslav <91912469+Artoriac@users.noreply.github.com>
2023-08-17 17:46:57 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,9 @@
+from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_add_btn(browser):
+    browser.get(link)
+    add_button = len(browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket"))
+    assert add_button > 0, "Add button is not displayed"
